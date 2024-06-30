9ec29994edec5108bbb880666d289d3e18c15938
Blinsitou <tribalsarko@hotmail.com>
2024-06-30 17:59:11 -0500

    third commit

--- controller/userController.js
+++ controller/userController.js
@@ -1,33 +1,6 @@
 const userModel = require('../model/userModel');
 const userCtrl = {};
 
-// Ruta para el registro de usuarios
-/*router.post('/register', async (req, res) => {
-    try {
-        const body = req.body;
-        const newUser = new userModel(body);
-        await newUser.save();
-        res.status(201).send({ message: 'Registro exitoso', user: newUser });
-    } catch (error) {
-        res.status(400).send({ error: 'Error en el registro', details: error.message });
-    }
-});
-
-// Ruta para el inicio de sesión
-router.post('/login', async (req, res) => {
-    try {
-        const { nomuser, password } = req.body;
-        const user = await userModel.findOne({ nomuser, password });
-        if (user) {
-            res.send({ message: 'Autenticación satisfactoria' });
-        } else {
-            res.status(401).send({ error: 'Error en la autenticación' });
-        }
-    } catch (error) {
-        res.status(400).send({ error: 'Error en la autenticación', details: error.message });
-    }
-});
-*/
 
 //Crear
 userCtrl.createUser =async (req, res) => {
